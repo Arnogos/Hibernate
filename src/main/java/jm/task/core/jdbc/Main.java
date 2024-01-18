d712d2e92461990a588ac0ba1ee098ffe01c4f51
Arnogos <gleb.pechyoonkin@mail.ru>
2024-01-18 18:48:20 +0700

    service

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -2,6 +2,7 @@ package jm.task.core.jdbc;
 
 import jm.task.core.jdbc.dao.UserDao;
 import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
+import jm.task.core.jdbc.service.UserService;
 import jm.task.core.jdbc.service.UserServiceImpl;
 
 import java.sql.*;
@@ -9,15 +10,15 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) throws SQLException {
-        UserDao userDao = new UserDaoJDBCImpl();
-        userDao.createUsersTable();
-        userDao.saveUser("Poppy", "LoL", (byte) 24);
-        userDao.saveUser("Warwick", "LoL", (byte) 33);
-        userDao.saveUser("Kayn", "LoL", (byte) 20);
-        userDao.saveUser("Rhaast", "LoL", (byte) 1000);
-        userDao.getAllUsers();
-        userDao.cleanUsersTable();
-        userDao.dropUsersTable();
+        UserService userService = new UserServiceImpl();
+        userService.createUsersTable();
+        userService.saveUser("Poppy", "LoL", (byte) 24);
+        userService.saveUser("Warwick", "LoL", (byte) 33);
+        userService.saveUser("Kayn", "LoL", (byte) 20);
+        userService.saveUser("Rhaast", "LoL", (byte) 1000);
+        userService.getAllUsers();
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
 
         }
     }
